e77486e1aac5c26c7d4dd68d29c487d057626a49
Jian Ma <jianquanma@gmail.com>
2025-03-21 19:13:35 -0400

    Initial Commit

---
+++ Synchronoss/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Synchronoss
+//
+//  Created by Jian Ma on 3/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Synchronoss/SynchronossApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynchronossApp.swift
+//  Synchronoss
+//
+//  Created by Jian Ma on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct SynchronossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
